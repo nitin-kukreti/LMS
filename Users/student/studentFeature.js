bceecca072dd6921544f08bd5de6bef53ccf1102
Nitin <nitinkukreti12@gmail.com>
2023-04-17 20:18:32 +0530

    basis feature of student initilize

---
+++ Users/student/studentFeature.js
@@ -0,0 +1,23 @@
+const { student: Student } = require('../UserModel')
+
+
+
+//  user can access courses which is included in its batch
+const getCources = (req, res) => {
+    res.json([])
+}
+
+const getCourseDetail = (req, res) => {
+
+}
+
+
+const streamVideo = (req, res) => {
+
+}
+
+
+module.exports = {
+    getCources, getCourseDetail, streamVideo
+}
+
