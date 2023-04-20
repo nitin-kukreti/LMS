69072555ab5e35863db3ccc63a4834da7cc7b7ab
Nitin <nitinkukreti12@gmail.com>
2023-04-20 14:24:28 +0530

    middelware for checking wether request made by user or not

---
+++ Users/admin/middleware/validAdmin.js
@@ -0,0 +1,31 @@
+
+const jwt = require('jsonwebtoken')
+const validAdmin = (req, res, next) => {
+    var tokenheader = req.headers['authorization'];
+    if (tokenheader !== undefined) {
+        try {
+            var data = tokenheader.split(" ");
+            const token = data[1];
+            jwt.verify(token, "key", (error, authdata) => {
+                if (error) {
+                    res.json(401, { msg: "incorrect token is provided" })
+                    return;
+                }
+                if (authdata.userType !== 'admin') {
+                    res.json(401, { msg: "you are not admin" }) 
+                    return;
+                }
+                next();
+            })
+
+        } catch (error) {
+            res.json(401, { msg: "recheck token formate" })
+        }
+
+    } else {
+        res.json(401, { msg: "token is not provided" })
+    }
+
+};
+
+module.exports = validAdmin;
