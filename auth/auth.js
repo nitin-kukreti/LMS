43d5da10f8374cab4a88c303cbb5f38e99df9fef
Nitin <nitinkukreti12@gmail.com>
2023-04-21 18:48:27 +0530

    improve code quality

--- auth/auth.js
+++ auth/auth.js
@@ -1,12 +1,12 @@
 const bcrypt = require('bcrypt');
 const { student } = require('../Users/userModel');
 const JWT=require('jsonwebtoken');
-const auth = async (req, res) => {
+const authenticate = async (req, res) => {
     const { email, password } = req.body;
-    const data = await student.findOne({ email:email });
-    if (data) {
-        if (bcrypt.compare(password, data.password)) {
-            const token=await JWT.sign(data,"key");
+    const user = await student.findOne({ email:email });
+    if (user) {
+        if (bcrypt.compare(password, user.password)) {
+            const token=await JWT.sign(user,"key");
             res.json(200,{
                 msg:"sign in successfully",
                 token:token
