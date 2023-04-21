4828bb73dcb1a13115f3bd1f5be4e90847096668
Nitin <nitinkukreti12@gmail.com>
2023-04-21 14:50:17 +0530

    auth feature added

---
+++ auth/auth.js
@@ -0,0 +1,27 @@
+const bcrypt = require('bcrypt');
+const { student } = require('../Users/userModel');
+const JWT=require('jsonwebtoken');
+const auth = async (req, res) => {
+    const { email, password } = req.body;
+    const data = await student.findOne({ email:email });
+    if (data) {
+        if (bcrypt.compare(password, data.password)) {
+            const token=await JWT.sign(data,"key");
+            res.json(200,{
+                msg:"sign in successfully",
+                token:token
+            })
+            
+        } else {
+            res.json(404,{msg:"password not match"})
+        }
+
+    }else{
+        res.json(404,{msg:"user not found check your name"})
+    }
+
+}
+
+module.exports=auth;
+
+
