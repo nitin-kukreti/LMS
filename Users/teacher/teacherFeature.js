cf961f85065a3e902d573c184941d0b007f62c91
Nitin <nitinkukreti12@gmail.com>
2023-04-25 14:14:03 +0530

    upload assignment feature implemnted

--- Users/teacher/teacherFeature.js
+++ Users/teacher/teacherFeature.js
@@ -1,5 +1,45 @@
+const { Batch } = require("mongodb");
 
-const uploadAssignment = (req, res) => {
-     res.send("needs to be implemented")
-     
-}
+const uploadAssignment = async (req, res) => {
+     const { batchId, title, description, dueDate } = req.body;
+     const teacherId = req.user._id; // Assuming teacher is logged in
+   
+     try {
+       const batch = await Batch.findById(batchId);
+       if (!batch) {
+         return res.status(404).json({ error: 'Batch not found' });
+       }
+       
+       if (!batch.teachers.includes(teacherId)) {
+         return res.status(401).json({ error: 'You are not authorized to upload assignments to this batch' });
+       }
+   
+       const files = req.files;
+       const attachments = [];
+       if (files) {
+         // Save attachments to disk and add their paths to attachments array
+         for (const key in files) {
+           const file = files[key];
+           const fileName = `${batchId}_${Date.now()}_${file.name}`;
+           await file.mv(`./uploads/${fileName}`);
+           attachments.push(fileName);
+         }
+       }
+   
+       const assignment = {
+         title,
+         description,
+         deadline: dueDate,
+         createdBy: teacherId,
+         attachments
+       };
+   
+       batch.assignments.push(assignment);
+       await batch.save();
+
+       res.status(201).json({ message: 'Assignment uploaded successfully' });
+     } catch (err) {
+       console.error(err);
+       res.status(500).json({ error: 'Server error' });
+     }
+   };
